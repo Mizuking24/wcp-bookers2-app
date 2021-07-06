171ea7993602f91c4d748430ff50ff1fa1b0f74f
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-07-06 10:02:04 +0000

    [Finish] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,27 +2,45 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = current_user
   end
 
   def create
+    @user = current_user
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+       flash[:notice] = "You have created book successfully."
+       redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @book1 = Book.find(params[:id])
+    @user = @book1.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+        flash[:notice] = "You have updated user successfully."
+        redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5709244b1352b96153ca4e17f17bc58375a4db99ca282cd3f3ebde6a0f33711e7884b0f88afcaf6db7bf9686c9bedb14bc6547d2d6557878a8fa8b20fbfe6080'
